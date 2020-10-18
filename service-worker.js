4b1dcd2c1eb5edac09a1830dfe9f476736801602
Kumar Gaurav <gauravpsp@gmail.com>
2020-10-18 20:30:27 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Algorand-Wallet/precache-manifest.915ae1f753b280c0f0e04ee489e1c721.js"
+  "/Algorand-Wallet/precache-manifest.404d11812768d81f9d287e534b96e913.js"
 );
 
 self.addEventListener('message', (event) => {
